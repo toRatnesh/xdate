6f366bba0106ef05a1f1ffb356a2b167e7a95934
Ratnesh Tiwari <48280625+ratnesh-tiwari@users.noreply.github.com>
2019-12-11 18:19:21 +0530

    removed main function
    removed main function from date_time function so that it can be used separately

--- xdate.c
+++ xdate.c
@@ -1,28 +1,6 @@
 
 #include "xdate.h"
 
-
-int main(int argc, char * agv[]) {
-
-	char tm_buf[TBUF_LEN] = {0};
-
-	date_time(DD, tm_buf);			fprintf(stdout, "%s\n", tm_buf);	
-	date_time(MN, tm_buf);			fprintf(stdout, "%s\n", tm_buf);
-	date_time(YY, tm_buf);			fprintf(stdout, "%s\n", tm_buf);
-	date_time(YYYY, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(DDMMYY, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(DDMMYYYY, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(YYMMDD, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(YYYYMMDD, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(HH, tm_buf);			fprintf(stdout, "%s\n", tm_buf);
-	date_time(MM, tm_buf);			fprintf(stdout, "%s\n", tm_buf);
-	date_time(SS, tm_buf);			fprintf(stdout, "%s\n", tm_buf);
-	date_time(HHMMSS, tm_buf);		fprintf(stdout, "%s\n", tm_buf);
-	date_time(YYYYMMDDHHMMSS, tm_buf);	fprintf(stdout, "%s\n", tm_buf);
-	date_time(DDMMYYYYHHMMSS, tm_buf);	fprintf(stdout, "%s\n", tm_buf);
-
-	return 0;
-}
 void date_time(date_time_frmt_t dt_fmt, char * out_buf){
 
 	char tm_buf[TBUF_LEN] = {0};
